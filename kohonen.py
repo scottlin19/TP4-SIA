d92fe2521916de6b9cb7870b28de6d00c383db5f
Euge <epineiro@itba.edu.ar>
2021-05-08 11:46:30 -0300

    feat kohonen added

--- kohonen.py
+++ kohonen.py
@@ -11,6 +11,12 @@ class KohonenNeuron:
     def __init__(self,weights):
         self.weights = weights
 
+    def __repr__(self):
+        return f"Neuron - w: {self.weights}\n"
+    
+    def __str__(self):
+        return f"Neuron - w: {self.weights}\n"
+
 
 class Kohonen:
     def __init__(self, training_set, grid_dimension, radius, learning_rate=1):
@@ -19,70 +25,73 @@ class Kohonen:
         self.grid_dimension = grid_dimension
         self.learning_rate = learning_rate
 
-        if(radius[0] == true):
-            self.radius = radius[1]
-        else: 
-            self.radius = grid_dimension
-            
-        self.neurons = []
+        # if(radius[0] == true):
+        #     self.radius = radius[1]
+        # else: 
+        #     self.radius = grid_dimension
+        self.radius = radius    
+        self.neurons = [None] * grid_dimension
         #initialize weights 
         for i in range(self.grid_dimension):
-            self.neurons[i] = []
+            self.neurons[i] = [None] * grid_dimension
             for j in range(self.grid_dimension):
                 w = np.random.default_rng().uniform(0,1,self.input_dimension) #TODO cambiar a la otra opcion con un flag
                 self.neurons[i][j] = KohonenNeuron(w)
 
     def train(self,epochs):
         print(self.neurons)
-        
+        tr_length = len(self.training_set)
+        activations = [[0] * self.grid_dimension] * self.grid_dimension
+
         for i in range(epochs):
             aux_training = self.training_set.copy()
-
+            self.learning_rate = 1  # Cada vez que entramos a la iteracion, reiniciamos el eta
+            aux_radius = self.radius
             while len(aux_training) > 0: 
                 i_x = np.random.randint(0, len(aux_training))               # get random input
                 input_ = aux_training[i_x]
                 aux_training = np.delete(aux_training, i_x, axis=0)
 
                 
-                (x,y,winner_neuron) = self.get_winner_neuron(input_) # search winner neuron 
-
-                update_weights(x,y,winner_neuron)               # update neighbours and curr neuron weights 
-          
-    #[raiz(2)  1    raiz(2)      
-    # 1        0    1         2
-    #]raiz(2)  1    raiz(2)   x
-    # raiz((abs(x-x'))^2 + (abs(y-y'))^2)
-
-    # (3,2)    (1,2,3,4,5,6,7)
-    # (2,1)
-    #raiz((3-2)^2 + (2,1)^2)
-    # (4,4)
-    #raiz((1)^2 + (2)^2)
-
+                (x,y,winner_neuron) = self.get_winner_neuron(input_)        # search winner neuron 
+                activations[x,y] += 1
+                self.update_weights(x,y,winner_neuron,input_)               # update neighbours and curr neuron weights 
+                
+                if(aux_radius > 1):
+                    aux_radius -= 1
+                self.learning_rate = 1/(tr_length - len(aux_training))
+         
+        # Graficar mapa de calor con activaciones
+        
+        # Graficar matriz U  
+        print("---------------- END ------------------")
+        print(self.neurons)
      
-    def update_weights(x,y,winner_neuron):
+    def update_weights(self,x,y,winner_neuron,input_):
         # Iterar por todos las neuronas
             # Calcular su distancia al ganador 
             # Si dist < R: cambiar peso (es vecina)
             # Sino nada
-    def get_neighbours(winner_neuron):
-         
-
+        for i,row in enumerate(self.neurons):
+            for j,neuron in enumerate(row): 
+                if self.input_distance((i,j), (x,y)) <= self.radius: #x,y es la pos de la winner neuron -->  es vecina
+                    neuron.weights += self.learning_rate * (input_ - neuron.weights)
 
     def get_winner_neuron(self,input_):
         to_return = None # neuron position in output grid 
+        min_dist = float('inf')
         for i,row in enumerate(self.neurons):
             for j,neuron in enumerate(row):
                 
-                dist = input_distance(input_, neuron.weights)
+                dist = self.input_distance(input_, neuron.weights)
                 if dist < min_dist: 
                     min_dist = dist
-                    winner_neuron = neurons[i][j]
+                    winner_neuron = self.neurons[i][j]
                     to_return = (i,j,winner_neuron)
         return to_return
 
             
-    def input_distance(p1,p2):
+    def input_distance(self,p1,p2):
         dif_squares = [(v1 - v2)**2 for v1,v2 in zip(p1,p2)]
         return math.sqrt(sum(dif_squares))
     
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from oja import run_oja
+from kohonen import run_kohonen
 import pandas as pd
 import numpy as np
 import json
@@ -7,8 +8,9 @@ from sklearn.preprocessing import StandardScaler
 with open("config.json") as f:
     config = json.load(f)
 
-learning_rate = config["learning_rate"]
-epochs_amount = config["epochs_amount"]
+# learning_rate = config["learning_rate"]
+# epochs_amount = config["epochs_amount"]
+
 
 def load_csv(): 
     
@@ -26,6 +28,12 @@ if exercise == '1':
     training_set = load_csv()
 
     type_ = input('Select \'1\' for Kohonen or \'2\' for Oja: ')
+    if type_ == '1':
+        grid_dimension = config["ej1"]["kohonen"]["grid_dimension"]
+        radius_value = config["ej1"]["kohonen"]["radius_value"]
+        learning_rate = config["ej1"]["kohonen"]["learning_rate"]
+        epochs_amount = config["ej1"]["kohonen"]["epochs_amount"]
+        run_kohonen(training_set, grid_dimension, radius_value, learning_rate, epochs_amount)
     if type_ == '2':
         w = run_oja(training_set, learning_rate, epochs_amount)
         print(w)
